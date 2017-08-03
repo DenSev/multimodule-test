6ce6a22c0fc54d1affd91c849d1cdd5798f5f5d1
DenSev <Den.Sev@outlook.com>
2017-08-03 14:46:19 +0300

    updates

--- multimodule-aop/src/main/java/com/densev/multimodule/aop/AopApp.java
+++ multimodule-aop/src/main/java/com/densev/multimodule/aop/AopApp.java
@@ -1,5 +1,6 @@
 package com.densev.multimodule.aop;
 
+import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +22,45 @@ public class AopApp {
     private RepositoryTest repositoryTest;
 
     public static void main(String... args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-        AopApp application = context.getBean(AopApp.class);
-        application.run();
+        try (ServerWrapper server = new ServerWrapper(Server.createTcpServer(args).start())) {
+            ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+            AopApp application = context.getBean(AopApp.class);
+            application.run();
+            LOG.info("H2 server is running at port: {}, with status: {}", server.getPort(), server.getStatus());
+        } catch (Exception exception) {
+            LOG.error("Caught exception:", exception);
+        }
+
     }
 
 
     public void run() {
         LOG.info(repositoryTest.wrapperSearch("something something something test"));
         logContainer.log();
+    }
+
+    public static class ServerWrapper implements AutoCloseable {
+
+        private static final Logger LOG = LoggerFactory.getLogger(ServerWrapper.class);
+        private final Server server;
+
+        public ServerWrapper(Server server) {
+            this.server = server;
+        }
+
+        @Override
+        public void close() throws Exception {
+            LOG.debug("Stopping server...");
+            server.stop();
+            LOG.debug("Server stopped.");
+        }
+
+        public int getPort() {
+            return server.getPort();
+        }
+
+        public String getStatus() {
+            return server.getStatus();
+        }
     }
 }
