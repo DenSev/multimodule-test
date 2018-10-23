e3425419de3348ff207f3d68b1b52daffc1b299a
DenSev <den.sev@outlook.com>
2018-10-23 23:07:02 +0300

    Raname file to cell

--- multimodule-chess/src/test/java/com/densev/chess/CellTest.java
+++ multimodule-chess/src/test/java/com/densev/chess/CellTest.java
@@ -1,7 +1,7 @@
 package com.densev.chess;
 
+import com.densev.chess.game.board.Cell;
 import com.densev.chess.game.board.Color;
-import com.densev.chess.game.board.File;
 import com.densev.chess.game.board.Piece;
 import org.testng.annotations.Test;
 
@@ -12,13 +12,13 @@ import static org.testng.Assert.assertTrue;
 /**
  * Created on: 10/23/18
  */
-public class FileTest {
+public class CellTest {
 
 
     @Test
     public void testNotOpposing() {
-        File x = File.with(Piece.BISHOP, Color.BLACK);
-        File y = File.empty();
+        Cell x = Cell.with(Piece.BISHOP, Color.BLACK);
+        Cell y = Cell.empty();
 
         assertTrue(y.isNotOpposing(x));
         assertTrue(x.isNotOpposing(y));
@@ -26,8 +26,8 @@ public class FileTest {
 
     @Test
     public void testOpposing() {
-        File x = File.with(Piece.BISHOP, Color.BLACK);
-        File y = File.empty();
+        Cell x = Cell.with(Piece.BISHOP, Color.BLACK);
+        Cell y = Cell.empty();
 
         assertFalse(y.isOpposing(x));
         assertFalse(x.isOpposing(y));
